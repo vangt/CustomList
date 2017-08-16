ab426c9c44cec549f7d4e44bccedd19a36127f90
vangt <vangt86@gmail.com>
2017-08-16 13:45:46 -0500

    Added tc to increase list size and add object.

--- CustomNewListUnitTesting/UnitTest1.cs
+++ CustomNewListUnitTesting/UnitTest1.cs
@@ -12,12 +12,41 @@ namespace CustomNewListUnitTesting
         {
             //Arrange
             CustomNewList<string> customList = new CustomNewList<string>();
-            string stringOne = "Tim";
+            int expectedResult = 0;
+
+            //Act
+            int result = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Add_Array_IncreaseSize()
+        {
+            //Arrange
+            CustomNewList<string> customList = new CustomNewList<string>();
+            int increaseBy = 1;
             int expectedResult = 1;
 
             //Act
-            customList[0] = stringOne;
+            customList.Capcity(1);
             int result = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Add_Array_ObjectAdd()
+        {
+            //Arrange
+            CustomNewList<string> customList = new CustomNewList<string>();
+            string name = "Tim";
+            string expectedResult = "Tim";
+
+            //Act
+            string result = customList[0];
 
             //Assert
             Assert.AreEqual(expectedResult, result);
