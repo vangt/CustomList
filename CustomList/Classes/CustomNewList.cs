249b1046ec2a5c39e32cc3fcf209455a7ef6e690
vangt <vangt86@gmail.com>
2017-08-17 15:04:04 -0500

    Created an array, count, and capacity member variable.  Added list constructor, indexer, Count, Capacity properties, and a started GetCount method.

--- CustomList/Classes/CustomNewList.cs
+++ CustomList/Classes/CustomNewList.cs
@@ -8,6 +8,54 @@ namespace CustomList.Classes
 {
     public class CustomNewList<T>
     {
-       
+        public T[] array;
+        private int count = 0;
+        private int capacity = 0;
+
+        public CustomNewList()
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+
+        public void GetCount()
+        {
+
+        }
     }
 }
